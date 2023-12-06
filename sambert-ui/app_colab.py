2b2f4794a02449eaa7a1a64c790d04346df18a7e
不做了睡大觉 <64798754+stakeswky@users.noreply.github.com>
2023-12-06 14:47:55 +0800

    Update app_colab.py

--- sambert-ui/app_colab.py
+++ sambert-ui/app_colab.py
@@ -11,9 +11,8 @@ def refresh():
 # gradio server ---------------------------
 with gr.Blocks() as server:
   # 面板说明
-  gr.Markdown("# <center>🌊💕🎶 Sambert UI 声音克隆</center>")
+  gr.Markdown("# <center>🌊💕🎶 LMU 声音克隆</center>")
   gr.Markdown("## <center>🌟 - 训练5分钟，通话不限时！AI真实拟声，支持中英双语！ </center>")      
-  gr.Markdown("### <center>🍻 - 更多精彩应用，尽在[滔滔AI](http://www.talktalkai.com)；滔滔AI，为爱滔滔！💕</center>")
 
   # 标记
   gr.Markdown('## 数据标注')
